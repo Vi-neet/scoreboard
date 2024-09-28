a8724d972d66f1f143be5aa4f844b60f0d184c14
vin <vin@vin.vin>
2024-09-28 12:44:22 +0530

    added reset functionality

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
 const homeTeamScore = document.querySelector(".home-team--score");
 const awayTeamScore = document.querySelector(".away-team--score");
+const resetBtn = document.querySelector(".reset");
 
 let homeScore = 0;
 let awayScore = 0;
 
-// HOME 
+// HOME
 function increaseHomeScorebyOne() {
   homeScore++;
   homeTeamScore.textContent = homeScore;
 }
 function increaseHomeScorebyTwo() {
-  homeScore+=2;
+  homeScore += 2;
   homeTeamScore.textContent = homeScore;
 }
 function increaseHomeScorebyThree() {
-    homeScore+=3;
-    homeTeamScore.textContent = homeScore;
-  }
+  homeScore += 3;
+  homeTeamScore.textContent = homeScore;
+}
 
-// AWAY 
+// AWAY
 
 function increaseAwayScorebyOne() {
-    awayScore++;
-    awayTeamScore.textContent = awayScore;
-  }
-  
+  awayScore++;
+  awayTeamScore.textContent = awayScore;
+}
+
 function increaseAwayScorebyTwo() {
-  awayScore+=2;
+  awayScore += 2;
   awayTeamScore.textContent = awayScore;
 }
 function increaseAwayScorebyThree() {
-  awayScore+=3;
+  awayScore += 3;
   awayTeamScore.textContent = awayScore;
 }
 
+function resetScore() {
+  awayScore = 0;
+  homeScore = 0;
+  awayTeamScore.textContent = awayScore;
+  homeTeamScore.textContent = homeScore;
+}
