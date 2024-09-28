4c9bce5dd1edf75e41a7bf76515c2e63cdfcaccf
vin <vin@vin.vin>
2024-09-28 12:32:34 +0530

    scoreboard working

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-const homeTeamScore= document.querySelector(".home-team--score");
-const awayTeamScore= document.querySelector(".away-team--score");
+const homeTeamScore = document.querySelector(".home-team--score");
+const awayTeamScore = document.querySelector(".away-team--score");
+
+let homeScore = 0;
+let awayScore = 0;
+
+// HOME 
+function increaseHomeScorebyOne() {
+  homeScore++;
+  homeTeamScore.textContent = homeScore;
+}
+function increaseHomeScorebyTwo() {
+  homeScore+=2;
+  homeTeamScore.textContent = homeScore;
+}
+function increaseHomeScorebyThree() {
+    homeScore+=3;
+    homeTeamScore.textContent = homeScore;
+  }
+
+// AWAY 
+
+function increaseAwayScorebyOne() {
+    awayScore++;
+    awayTeamScore.textContent = awayScore;
+  }
+  
+function increaseAwayScorebyTwo() {
+  awayScore+=2;
+  awayTeamScore.textContent = awayScore;
+}
+function increaseAwayScorebyThree() {
+  awayScore+=3;
+  awayTeamScore.textContent = awayScore;
+}
 
